7608ed97874a258c8043fd6939f8c76363d40eea
magicglass177 <magicglass957@gmail.com>
2026-09-25 16:21:51 +0700

    refactor: use session login route for SPA

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,14 +7,7 @@ use Illuminate\Support\Facades\{Gate, Route};
 Route::pattern('ticket', '[0-9]+');
 
 Route::prefix('v1')->name('api.v1.')->group(function () {
-    Route::post('auth/login', [AuthController::class, 'login'])
-        ->middleware('throttle:api-login')
-        ->name('login');
-
     Route::middleware(['auth:sanctum', 'throttle:api-v1'])->group(function () {
-        Route::post('auth/logout', [AuthController::class, 'logout'])
-            ->name('logout');
-
         Route::get('me', [AuthController::class, 'me'])
             ->name('me');
 
